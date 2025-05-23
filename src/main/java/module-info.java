8261ac029803cacf844d77082f62857a3f63946f
alekseytrust <110543488+alekseytrust@users.noreply.github.com>
2025-05-23 15:25:42 -0400

    Start View completed and fully functional. Dynamic elements and theme changing.

---
+++ src/main/java/module-info.java
@@ -0,0 +1,12 @@
+module test.truinconv {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+    requires org.controlsfx.controls;
+    requires net.synedra.validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+
+    opens test.truinconv to javafx.fxml;
+    exports test.truinconv;
+}
